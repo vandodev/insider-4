86be21046e26fc2525a17865a6be3e56d09bd8a4
Evandro oliveira <vandoaparecido@hotmail.com>
2021-09-27 23:30:44 -0300

    :rocket: Criando a navegação

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Home() {
+function Home() {
   return (
     <View>
       <Text>Home</Text>
--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Movies() {
+function Movies() {
   return (
     <View>
       <Text>Movies</Text>
