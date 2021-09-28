a0599d1d8d4bb529f03e362d934a43ed1a1a2a99
Evandro oliveira <vandoaparecido@hotmail.com>
2021-09-28 00:21:31 -0300

    :rocket:Personalizando o Drawer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import StackRoutes from "./stackRoutes";
 
@@ -9,9 +10,47 @@ const Drawer = createDrawerNavigator();
 
 function Routes() {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
-      <Drawer.Screen name="Movies" component={Movies} />
+    <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+        drawerStyle: {
+          backgroundColor: "#090A0E",
+          paddingTop: 20,
+        },
+        drawerActiveBackgroundColor: "#E72F49",
+        drawerActiveTintColor: "#FFF",
+        drawerInactiveTintColor: "#FFF",
+      }}
+    >
+      <Drawer.Screen
+        name="HomeDrawer"
+        component={StackRoutes}
+        options={{
+          title: "Inicio",
+          drawerIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              // name="movie-open"
+              name={focused ? "movie-open" : "movie-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Movies"
+        component={Movies}
+        options={{
+          title: "Meus filmes",
+          drawerIcon: ({ focused, size, color }) => (
+            <MaterialCommunityIcons
+              name={focused ? "archive" : "archive-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
